07a8680138304b4dd96ece2f6edbd4e043fe7d6f
Bin Tran <sudocode.bin@gmail.com>
2017-05-23 16:26:24 -0700

    changed app to server.js and editted files and added nodemon to handle node server rendering

--- server.js
+++ server.js
@@ -5,21 +5,34 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var appRoutes = require('./routes/app');
-var messageRoutes = require('./routes/messages');
+// Set Route variables
+var indexRoutes = require('./routes/index');
+var tasksRoutes = require('./routes/tasks');
 
+// Initialize node express app
 var app = express();
 
+// Connect Mongoose to Mongodb client on port 27017
 mongoose.connect('localhost:27017/ng-app');
 
+// View Engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.use(logger('dev'));
+
+// Body Parser MiddleWare
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Cookie Parser
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Set Headers
 app.use(function(req,res,next){
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -27,11 +40,17 @@ app.use(function(req,res,next){
   next();
 });
 
-app.use('/message', messageRoutes);
-app.use('/', appRoutes);
+///////////// Connect Routes //////////////
 
-app.use(function(req,res,next){
-  return res.render('index');
+app.use('/message', tasksRoutes);
+app.use('/', indexRoutes);
+
+
+// Set and listen on port
+
+var port = 3000;
+app.listen(port, function(){
+  console.log('Server started on port ' + port);
 });
 
 module.exports = app;
