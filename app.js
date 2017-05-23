803eacb6cf3ad2b3dcf760f9b03596fe1e0b47b4
Bin Tran <sudocode.bin@gmail.com>
2017-05-23 15:55:47 -0700

    Node js app.js file to get server ready

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var appRoutes = require('./routes/app');
+var messageRoutes = require('./routes/messages');
+
+var app = express();
+
+mongoose.connect('localhost:27017/ng-app');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req,res,next){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+
+app.use('/message', messageRoutes);
+app.use('/', appRoutes);
+
+app.use(function(req,res,next){
+  return res.render('index');
+});
+
+module.exports = app;
